0e00c803ebb246bde1a485025000a3058fd9b27d
johnpospisil <audiofreak7@gmail.com>
2019-05-14 12:56:44 -0700

    Move 'getCurrentPosition' of user to the App constructor

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props); // needed for every constructor
-    this.state = { lat: null }; // initially set latitude to 'null'
+    this.state = { lat: null }; // initially set latitude to 'null'. Can only use '=' to initially 
+    // set state in constructors.
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // call 'setState' to update the state of the latitude
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   // react says that we have to define render!!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
+    
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
