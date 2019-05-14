f7775ca413baca03a8df9d20cb42ea4ba7cd7d50
johnpospisil <audiofreak7@gmail.com>
2019-05-14 12:30:37 -0700

    Add a constructor to the App. Initialing state through constructors.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props); // needed for every constructor
+    this.state = { lat: null }; // initially set latitude to 'null'
+  }
+
+  // react says that we have to define render!!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
